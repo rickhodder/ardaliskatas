77b5de15bd407bd030880dc4cd4a38e69eecc493
rickhodder <rickhodder@msn.com>
2019-10-10 23:43:09 -0400

    Refactor extract method checking for single die value in roll

--- Greed/Game.cs
+++ Greed/Game.cs
@@ -8,11 +8,9 @@ namespace Greed
         {
             var result = 0;
 
-            if (roll.Any(r => r == 1))
-                result+= 100;
+            result += ScoreSingle( 1, roll, 100);
 
-            if (roll.Any(r => r == 5))
-                result+= 50;
+            result += ScoreSingle(5, roll, 50);
 
             if (roll.Count(r => r == 1)>=3)
                 result += 1000;
@@ -21,6 +19,14 @@ namespace Greed
                 result += 200;
 
             return result;
+        }
+
+        private static int ScoreSingle( int die, int[] roll, int score)
+        {
+            if (roll.Any(r => r == die))
+                return score;
+
+            return 0;
         }
     }
 }
