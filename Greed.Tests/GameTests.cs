dd6c7c8aa5f12daa15a42b450ed843a2953c8d93
rickhodder <rickhodder@msn.com>
2019-10-10 22:54:31 -0400

    green - contains 5 returns 50

--- Greed.Tests/GameTests.cs
+++ Greed.Tests/GameTests.cs
@@ -14,6 +14,14 @@ namespace Greed.Tests
             Assert.Equal(100, score);
         }
 
+        [Fact]
+        public void ShouldScoreRollContainingFive_50()
+        {
+            var game = new Game();
+            var roll = new int[] { 5, 2, 3, 4, 4 };
+            var score = game.Score(roll);
+            Assert.Equal(50, score);
+        }
     }
 
 }
--- Greed/Game.cs
+++ Greed/Game.cs
@@ -1,9 +1,13 @@
-﻿namespace Greed
+﻿using System.Linq;
+
+namespace Greed
 {
     public class Game
     {
         public int Score(int[] roll)
         {
+            if (roll.Any(r => r == 5))
+                return 50;
             return 100;
         }
     }
