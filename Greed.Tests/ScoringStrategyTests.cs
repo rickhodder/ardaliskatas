154d251575b44dcfd534e5f97ba328717b859b06
rickhodder <rickhodder@msn.com>
2019-10-11 08:40:05 -0400

    Move towards strategy based scoring

--- Greed.Tests/ScoringStrategyTests.cs
+++ Greed.Tests/ScoringStrategyTests.cs
@@ -26,6 +26,22 @@ namespace Greed.Tests
         }
     }
 
+    public class MultipleScoringStrategyTests
+    {
+        [Fact]
+        public void ShouldReturnReturnScore_GivenMultipleDie()
+        {
+            var expectedScore = 2000;
+            var dieCount = 2;
+            var die = 4;
+            var strategy = new MultipleRollScoringStrategy(die,dieCount, expectedScore);
+            var roll = new Roll { 1, 2, 3, 4, 4 };
+            Assert.Equal(expectedScore, strategy.Score(roll));
+        }
+    }
+
+
+
     public class FakeRollScoringStrategy:IRollScoringStrategy
     {
         public int Score(Roll roll)
--- Greed/IRollScoringStrategy.cs
+++ Greed/IRollScoringStrategy.cs
@@ -28,4 +28,28 @@ namespace Greed
 
         }
     }
+
+    public class MultipleRollScoringStrategy : IRollScoringStrategy
+    {
+        private readonly int _die;
+        private readonly int _dieCount;
+        private readonly int _expectedScore;
+
+        public MultipleRollScoringStrategy(int die, int dieCount, int expectedScore)
+        {
+            _die = die;
+            _dieCount = dieCount;
+            _expectedScore = expectedScore;
+        }
+
+        public int Score(Roll roll)
+        {
+            if (roll.Count(r => r == _die) >= _dieCount)
+                return _expectedScore;
+
+            return 0;
+
+        }
+    }
+
 }
